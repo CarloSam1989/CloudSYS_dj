8b3dc5d349745fa2c5a7e6546fb2f1d4a7ec3697
CarloSam1989 <carlosgh89@hotmail.com>
2026-01-27 00:42:45 -0500

    est

--- erp_project/settings.py
+++ erp_project/settings.py
@@ -125,8 +125,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # 11. CONFIGURACIÓN DE CORREO
 # ==============================================================================
 # EMAIL CONFIGURATION (OUTLOOK)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
