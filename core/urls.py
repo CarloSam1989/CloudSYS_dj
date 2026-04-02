330db15303781e8a1bd3872292da153216146846
Carlos Guzman Hidalgo <carlosgh89@hotmail.com>
2026-04-02 11:44:23 -0500

    Reparacion de reseteo de clave

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 # Ubicación: core/urls.py
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 app_name = "core"
 urlpatterns = [
@@ -83,10 +83,41 @@ urlpatterns = [
     path('finanzas/prestamos/<int:pk>/abonar/', views.registrar_abono_prestamo, name='prestamo_abono'),
     path('mi-banco/', views.home_banking_view, name='home_banking'),
 
-    path('recuperar-password/',auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
-    path('recuperar-password/enviado/',auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'),name='password_reset_done'),
-    path('recuperar/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'),name='password_reset_confirm'),
-    path('recuperar/completado/',auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'),name='password_reset_complete'),
+    path(
+        'recuperar-password/',
+        auth_views.PasswordResetView.as_view(
+            template_name='auth/password_reset.html',
+            email_template_name='auth/password_reset_email.html',
+            subject_template_name='auth/password_reset_subject.txt',
+            success_url=reverse_lazy('core:password_reset_done'),
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'recuperar-password/enviado/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='auth/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'recuperar/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='auth/password_reset_confirm.html',
+            success_url=reverse_lazy('core:password_reset_complete'),
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'recuperar/completado/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='auth/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
     path('backup-db/', views.ejecutar_backup, name='backup_db'),
 
     path('inventario/importar/', views.importar_inventario_excel_view, name='inventario_importar'),
